a0f02c5f70f1c3802dd0d8f72537f086a4049c2d
arunsaradgi <saradgi07@gmail.com>
2024-07-17 12:13:06 +0530

    login api

--- backend/db/User.js
+++ backend/db/User.js
@@ -4,8 +4,6 @@ const userSchema = new Schema({
     name: String,
     email: String,
     password: String,
-    age:Number,
-    password:String,
 });
 
 module.exports = model("users", userSchema);
--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,22 @@ app.use(cors())
 app.post("/register", async (req, res) => {
     let user = new User(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     res.send(result);
+})
+
+app.post('/login', async (req, res) => {
+    if (req.body.email && req.body.password) {
+        let user = await User.findOne(req.body).select('-password');
+        if (user) {
+            res.send(user)
+        } else {
+            res.send("No user found")
+        }
+    } else {
+        res.send("Please enter valid email and password")
+    }
 })
 
 app.listen(5000)
